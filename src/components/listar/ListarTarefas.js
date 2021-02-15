965173cd43bbc210ed6f94962ec6e44897d5c317
thiagoalgo <thiagoalgo@gmail.com>
2021-02-15 17:55:48 -0300

    Implementado useEffects para buscar tarefas e alterada a estrutura de arquivos para cada componente ter sua propria pasta

--- src/components/listar/ListarTarefas.js
+++ src/components/listar/ListarTarefas.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { A } from 'hookrouter'
+import { Table } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function ListarTarefas() {
-    return (
-        <A href='/cadastrar'
-            className='btn btn-success btn-sm'>
-            Nova tarefa
-        </A>
-    )
+
+  const [tarefas, setTarefas] = useState([])
+  const [carregarTarefas, setCarregarTarefas] = useState(true)
+
+  useEffect(() =>{
+    function obterTarefas() {
+      const tarefasDb = localStorage['tarefas']
+      let listaTarefas = tarefasDb ? JSON.parse(tarefasDb): []
+      setTarefas(listaTarefas)
+      console.log(listaTarefas)
+    }
+    if (carregarTarefas) {
+      obterTarefas()
+      setCarregarTarefas(false)
+    }
+  }, [carregarTarefas])
+
+  return (
+    <div className="text-center">
+      <Table striped bordered hover responsive data-testid="tabela">
+        <thead>
+          <tr>
+            <th>
+              Tarefas
+            </th>
+            <th>
+              <A href='/cadastrar'
+                className='btn btn-success btn-sm'
+                data-testid="btn-nova-tarefa">
+                <FontAwesomeIcon icon={faPlus}/>
+                &nbsp;
+                Nova tarefa
+              </A>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </Table>
+    </div>
+  )
 }
 
 export default ListarTarefas
